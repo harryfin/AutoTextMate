314cca3901514934528ac0487e9b8e91bc70522d
Harry Findeis <harry.findeis@singular-it.de>
2024-09-22 17:10:04 +0200

    fix #-bug and refactor notes

--- paste_script.py
+++ paste_script.py
@@ -1,56 +1,110 @@
-import pyautogui
-import keyboard
 import time
 
+import keyboard
 
-# Definition der Textbausteine
+typed_chars = ''  # Puffer für die zuletzt eingegebenen Zeichen
+
+# Definition der Textbausteine (Auf Joplin-Notizen (Markdown) angepasst)
 Daily_Notes = """
-#Daily Notes - {}
+# Daily Notes - {}
 
 ## Tagesziele
-- [ ]
-- [ ] 
-- [ ]
+- [ ] Tagziel 1
+Tagziel 2
+Tagziel 3
 
 ## Aufgaben
-- [ ] 
-- [ ]
-- [ ]
+- [ ] Aufgabe 1
+Aufgabe 2
+Aufgabe 3
 """
 
+Meeting_Notes = """
 
-texts = {
+## Meetingtype - {}
+
+Meeting Details
+
+## Agenda
+1. Punkt 1
+Punkt 2
+Punkt 3
+
+
+## Protokoll
+### Punkt 1
+- Information 1
+Information 2
+
+
+### Punkt 2
+- Information 1
+Information 2
+
+
+## Zusammenfassung
+**Hauptentscheidungen**:
+- Entscheidung 1
+Entscheidung 2
+
+
+**Nächste Schritte**:
+- Schritt 1: [Verantwortlich: Name, Fälligkeitsdatum]
+Schritt 2: [Verantwortlich: Name, Fälligkeitsdatum]
+
+
+## Nächstes Meeting
+**Datum/Uhrzeit**: [Datum und Uhrzeit des nächsten Meetings]
+**Agenda-Vorschläge**:
+- Vorschlag 1
+Vorschlag 2
+"""
+
+# Definition der Ersetzungen
+replacements = {
     "#daily": Daily_Notes.format(time.strftime("%d-%m-%Y")),
-    "#todo": "1. Aufgabe\n2. Aufgabe\n3. Aufgabe",
-    "#meeting": "Besprechung am Montag um 10 Uhr.",
+    "#meeting": Meeting_Notes.format(time.strftime("%d-%m-%Y")),
 }
 
-# Funktion, um Text zu ersetzen
-def replace_text(typed_text, replacement_text):
-    # Lösche das getippte Kürzel (z.B. #todo)
-    for _ in range(len(typed_text)):
-        pyautogui.press('backspace')
-    # Schreibe den neuen Text
-    pyautogui.write(replacement_text)
+max_trigger_length = max(len(trigger_word) for trigger_word in replacements.keys())
 
-# Überwache die Tastatureingaben
-def monitor_input():
-    buffer = ""  # Buffer, um eingegebene Zeichen zu speichern
-    while True:
-        event = keyboard.read_event()
-        if event.event_type == keyboard.KEY_DOWN:
-            if event.name == 'space':
-                # Wenn eine Leertaste gedrückt wird, prüfe, ob ein Kürzel im Buffer ist
-                if buffer in texts:
-                    replace_text(buffer, texts[buffer])
-                buffer = ""  # Leere den Buffer nach einem Leerzeichen
-            elif event.name == 'backspace':
-                buffer = buffer[:-1]  # Entferne das letzte Zeichen aus dem Buffer
-            elif len(event.name) == 1:  # Normale Zeichen wie a-z
-                buffer += event.name
 
-# Starte die Überwachung
+def on_key_event(e):
+    global typed_chars
+    if e.event_type == 'down':
+        if e.name == 'backspace':
+            typed_chars = typed_chars[:-1]
+        elif e.name == 'space':
+            typed_chars += ' '
+        elif e.name == 'enter':
+            typed_chars += '\n'
+        elif len(e.name) == 1:
+            typed_chars += e.name
+        else:
+            # Andere Tasten ignorieren
+            pass
+
+        # Puffer auf maximale Schlüsselwortlänge beschränken
+        typed_chars = typed_chars[-max_trigger_length:]
+
+        # Überprüfen auf alle Schlüsselwörter
+        for trigger_word, replacement_text in replacements.items():
+            if typed_chars.endswith(trigger_word):
+                # Anzahl der zu löschenden Zeichen
+                num_backspaces = len(trigger_word)
+                # Lösche das Auslöser-Wort
+                for _ in range(num_backspaces):
+                    keyboard.press_and_release('backspace')
+                # Schreibe den Ersetzungstext
+                keyboard.write(replacement_text)
+                # Puffer leeren, um Seiteneffekte zu vermeiden
+                typed_chars = ""
+                break  # Nach erster Übereinstimmung abbrechen
+
+
 if __name__ == "__main__":
-    print("Überwache die Eingabe für Schlüsselwörter...")
-    monitor_input()
+    # Registrieren des Hooks
+    keyboard.hook(on_key_event)
 
+    # Skript läuft kontinuierlich
+    keyboard.wait()
