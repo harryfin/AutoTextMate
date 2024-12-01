269035493facc58a3aeebe0ff81743bed4ab9767
Harry Findeis <harry.findeis@singular-it.de>
2024-12-01 01:15:51 +0100

    add new template data and fix issues

--- auto_text_mate.py
+++ auto_text_mate.py
@@ -1,13 +1,16 @@
-import time
+import datetime
 from pathlib import Path
+
 import keyboard
 
 typed_chars = ''  # Buffer for the last typed characters
+
 
 # Function to read a text file
 def read_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
+
 
 # Function to load all text snippets from a directory
 def load_notes(directory):
@@ -19,6 +22,22 @@ def load_notes(directory):
             notes[trigger_word] = read_file(file_path)
     return notes
 
+
+def get_template_date() -> dict:
+    """
+    Returns template data with following keys:
+    - date: Current date in the format dd-mm-YYYY
+    - kw: Calendar week of the current date
+
+    Returns:
+        dict: Dictionary with the current date and calendar week
+
+    """
+    date = datetime.datetime.now().strftime("%d-%m-%Y")
+    kw = datetime.datetime.now().isocalendar()[1]
+    return {"date": date, "kw": kw}
+
+
 # Directory of text snippets relative to the script directory
 script_directory = Path(__file__).parent
 notes_directory = script_directory / 'notes'
@@ -27,6 +46,7 @@ replacements = load_notes(notes_directory)
 
 # Calculate the maximum trigger word length
 max_trigger_length = max(len(trigger_word) for trigger_word in replacements.keys())
+
 
 def on_key_event(e):
     global typed_chars
@@ -48,22 +68,25 @@ def on_key_event(e):
         # Limit the buffer to the maximum trigger word length plus one for the space or enter
         typed_chars = typed_chars[-(max_trigger_length + 1):]
 
+
 def check_and_replace():
     global typed_chars
     # Check for all trigger words
     for trigger_word, template_text in replacements.items():
         if typed_chars.strip().endswith(trigger_word):
             # Number of characters to delete
-            num_backspaces = len(trigger_word +1)  # +1 for the hashtag
+            num_backspaces = len(trigger_word + 1)  # +1 for the hashtag
             # Delete the trigger word
             for _ in range(num_backspaces):
                 keyboard.press_and_release('backspace')
-            # Write the replacement text with the current date
-            replacement_text = template_text.format(time.strftime("%d-%m-%Y"))
+
+            template_data = get_template_date()  # Get the current date and calendar week
+            replacement_text = template_text.format(**template_data)
             keyboard.write(replacement_text)
             # Clear the buffer to avoid side effects
             typed_chars = ""
             break  # Stop after the first match
+
 
 if __name__ == "__main__":
     # Register the hook
